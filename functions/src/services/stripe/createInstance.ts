5f086201112bb859e7a8d160a81cf463619328fb
Trey Huffine <treyhuffine@gmail.com>
2023-06-02 17:56:20 -0600

    Upgrade firebase

--- functions/src/services/stripe/createInstance.ts
+++ functions/src/services/stripe/createInstance.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const API_VERSION = '2022-08-01';
+const API_VERSION = '2022-11-15';
 
 const createInstance = () => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
